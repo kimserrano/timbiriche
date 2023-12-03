1253b15b0435f361ac45dafe967ab8cd45674e07
Erumeru <elpichikoala@gmail.com>
2023-12-03 13:08:39 -0700

    Los colores ahora se colorean desde tableroNegocio con un hashmap de los puertos

--- timbiriche/src/main/java/negocio/ITableroNegocio.java
+++ timbiriche/src/main/java/negocio/ITableroNegocio.java
@@ -16,7 +16,7 @@ public interface ITableroNegocio {
     public void generarTurnos(List<JugadorDTO> jugadores);
     public void otorgarPuntos();
     public void realizarMovimiento(BtnTimbi btn);
-    public void pintarMovimiento(int cordX, int cordY, boolean orientacion, String color);
+    public void pintarMovimiento(int cordX, int cordY, boolean orientacion, String color, int puerto);
     public void anotarPuntoLocal();
     public void actualizarAccionesTablero(List<JugadorDTO> jugadores) ;
 }
